386deff1a5dd6da49a91cf566363fd8ad5cf4758
Davi Azevedo Cabral <davizinhocanjica@gmail.com>
2026-03-07 12:37:48 -0300

    Integrate dotenv for environment variable management
    Added dotenv support for environment variable loading.

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ import base64
 from discord.ext import commands
 from discord.ui import Button, View, Modal, TextInput 
 
+from dotenv import load_dotenv
+
+
+
 # --- CONFIGURAÇÕES DO BOT ---
 intents = discord.Intents.default()
 intents.message_content = True
@@ -15,7 +19,7 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 # ==============================================================================
 # ⚠️ ÁREA DE CONFIGURAÇÃO ⚠️
-
+load_dotenv()
 # 1. TOKEN MP
 MP_ACCESS_TOKEN = os.getenv("TOKENMP")
 
@@ -275,7 +279,7 @@ async def loja(ctx):
         embed.set_image(url=IMAGEM_LOJA)
     embed.set_footer(text="Caboom's Store")
     await ctx.send(embed=embed, view=BotaoCompra())
-
+    
 bot.run(os.getenv("TOKEN"))
 
 
